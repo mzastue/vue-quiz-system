09c8dc93561744c76759265b637046767d337f80
mateusz.littwin <mzastue@gmail.com>
2019-02-16 12:13:46 +0100

    Remove old question view

--- src/components/Quiz/Question/Question.vue
+++ src/components/Quiz/Question/Question.vue
@@ -1,25 +1,17 @@
 <template>
-    <Box>
-      <div slot="text" class="question">
-        <h2>{{ question }}</h2>
-        <ul>
-          <li
+    <div slot="text" class="question">
+      <h2>{{ question }}</h2>
+      <ul>
+        <li
           v-for="(answer, index) in options"
           @click="$emit('clickAnswer', { answerNo: index + 1 })"
-          >{{ answer }}</li>
-        </ul>
-      </div>
-    </Box>
+        >{{ answer }}</li>
+      </ul>
+    </div>
 </template>
 
 <script>
-import Box from './../../Box/Box.vue';
-
 export default {
-  components: {
-    Box,
-  },
-
   props: {
     question: {
       type: String,
